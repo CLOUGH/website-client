714438cc102a623b585c44f2db4553025b5a04ae
Warren Clough <clough.warren@gmail.com>
2018-04-03 14:38:41 -0500

    fixed comipation issue with the map funtion

--- src/app/shared/services/post/post.service.ts
+++ src/app/shared/services/post/post.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Post } from '../../models/post';
 import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 
 @Injectable()
