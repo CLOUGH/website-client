d9a60a79f690e928df8070bb9d30baee4f24266b
Warren Clough <clough.warren@gmail.com>
2018-04-06 09:43:58 -0500

    added env to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://warrenclough.com:8443'
+  apiUrl: 'https://warrenclough.com:8443',
+  auth: {
+    clientID: 'YOUR-AUTH0-CLIENT-ID',
+    domain: 'YOUR-AUTH0-DOMAIN', // e.g., you.auth0.com
+    audience: 'YOUR-AUTH0-API-IDENTIFIER', // e.g., http://localhost:3001
+    redirect: 'http://warrenclough.com/callback',
+    scope: 'openid profile email'
+  }
 };
