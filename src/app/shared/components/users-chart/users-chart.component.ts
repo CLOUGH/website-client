573e249953bc858212d2e65e2fc30d68f3dfd4a5
Warren Clough <clough.warren@gmail.com>
2018-04-18 20:40:40 -0500

    added c3 and multiple dimension on user chart

--- src/app/shared/components/users-chart/users-chart.component.ts
+++ src/app/shared/components/users-chart/users-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
 import * as moment from 'moment';
+import { format } from 'url';
 
 @Component({
   selector: 'app-users-chart',
@@ -17,8 +18,10 @@ export class UsersChartComponent implements OnInit {
   private parseTime;
   private x;
   private y;
+  private z;
   private line;
   private _data;
+  private metrics = ['User', 'New User'];
 
   @Input('data') set data(data) {
     this._data = data;
@@ -33,7 +36,7 @@ export class UsersChartComponent implements OnInit {
 
   ngOnInit() {
     this.svg = d3.select(this.svgElement.nativeElement);
-    this.margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    this.margin = { top: 20, right: 150, bottom: 30, left: 50 };
     this.width = +this.svgElement.nativeElement.getBoundingClientRect().width - this.margin.left - this.margin.right;
     this.height = +this.svgElement.nativeElement.getBoundingClientRect().height - this.margin.top - this.margin.bottom;
     this.g = this.svg.append('g').attr('transform', `translate(${this.margin.left},${this.margin.top})`);
@@ -46,6 +49,8 @@ export class UsersChartComponent implements OnInit {
     this.y = d3.scaleLinear()
       .rangeRound([this.height, 0]);
 
+    this.z = d3.scaleOrdinal(d3.schemeCategory10);
+
     this.line = d3.line()
       .x((d: any) => this.x(d.date))
       .y((d: any) => this.y(d.users));
@@ -53,6 +58,7 @@ export class UsersChartComponent implements OnInit {
   }
 
   drawChart() {
+
     const data = this._data.reports[0].data.rows.map(d => {
       return {
         date: this.parseTime(d.dimensions[0]),
@@ -60,59 +66,100 @@ export class UsersChartComponent implements OnInit {
       };
     });
 
+    const users = this.metrics.map((metric, index) => {
+      return {
+        metric: metric,
+        values: this._data.reports[0].data.rows.map(d => {
+          return {
+            date: this.parseTime(d.dimensions[0]),
+            users: +d.metrics[0].values[index]
+          };
+        })
+      };
+    });
+    console.log(users);
+
     // build chart
     this.x.domain(d3.extent(data, (d: any) => d.date));
-    this.y.domain([0, d3.max(data, (d: any) => d.users) + 1]);
+    this.y.domain([0, d3.max(this._data.reports[0].data.maximums, (d: any) => d3.max(d.values, (value: any) => +value)) + 0.5]);
+    this.z.domain(users.map(c => c.metric));
 
     // add x axis grid lines
     this.g.append('g')
-      .attr('class', 'x-axis')
+      .attr('class', 'grid')
       .attr('transform', 'translate(0,' + this.height + ')')
       .call(
         d3.axisBottom(this.x)
-          // .tickSize(-this.height)
-          .tickFormat(d3.timeFormat('%Y-%m-%d'))
+          // .ticks(this._data.reports[0].data.rowCount)
+          .tickSize(-this.height)
+          .tickFormat(d3.timeFormat(''))
       )
       .select('.domain')
       .remove();
 
     // add the Y grid lines
     this.g.append('g')
-      .attr('class', 'y-axis')
+      .attr('class', 'grid')
       .call(
         d3.axisLeft(this.y)
           .tickSize(-this.width)
-          .ticks(d3.max(data, (d: any) => d.users) + 1)
-          .tickFormat(d3.format('d'))
+          .ticks(d3.max(this._data.reports[0].data.maximums, (d: any) => d3.max(d.values, (value: any) => +value)))
+          .tickFormat('')
       );
-    // .append('text')
-    // .attr('transform', 'rotate(-90)')
-    // .attr('y', 6)
-    // .attr('dy', '0.71em')
-    // .attr('text-anchor', 'end')
-    // .text('Users');
+    // add the X Axis
+    this.g.append('g')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(d3.axisBottom(this.x));
+
+    // add the Y Axis
+    this.g.append('g')
+      .call(
+        d3.axisLeft(this.y)
+          .ticks(d3.max(this._data.reports[0].data.maximums, (d: any) => d3.max(d.values, (value: any) => +value)))
+      );
 
     // Data line and dots group
-    const lineAndDots = this.g.append('g')
-      .attr('class', 'line-and-dots');
+    // const lineAndDots = this.g.append('g')
+    //   .attr('class', 'line-and-dots');
 
-    lineAndDots.append('path')
-      .datum(data)
-      .attr('fill', 'none')
-      .attr('stroke', '#3DA3E8')
+    // lineAndDots.append('path')
+    //   .datum(data)
+    //   .attr('fill', 'none')
+    //   .attr('stroke', '#3DA3E8')
+    //   .attr('stroke-linejoin', 'round')
+    //   .attr('stroke-linecap', 'round')
+    //   .attr('stroke-width', 3)
+    //   .attr('d', this.line);
+
+    // lineAndDots.selectAll('.dot')
+    //   .data(data)
+    //   .enter()
+    //   .append('circle') // Uses the enter().append() method
+    //   .attr('class', 'dot') // Assign a class for styling
+    //   .attr('cx', (d, i) => this.x(d.date))
+    //   .attr('cy', (d) => this.y(d.users))
+    //   .attr('r', 3);
+
+    const user = this.g.selectAll('.users')
+      .data(users)
+      .enter().append('g')
+      .attr('class', 'user');
+
+    user.append('path')
+      .attr('class', 'line')
+      .attr('d', d => this.line(d.values))
       .attr('stroke-linejoin', 'round')
       .attr('stroke-linecap', 'round')
       .attr('stroke-width', 3)
-      .attr('d', this.line);
+      .style('stroke', d => this.z(d.metric));
 
-    lineAndDots.selectAll('.dot')
-      .data(data)
-      .enter()
-      .append('circle') // Uses the enter().append() method
-      .attr('class', 'dot') // Assign a class for styling
-      .attr('cx', (d, i) => this.x(d.date))
-      .attr('cy', (d) => this.y(d.users))
-      .attr('r', 3);
+    user.append('text')
+      .datum((d) => ({ metric: d.metric, value: d.values[d.values.length - 1] }))
+      .attr('transform', (d) => 'translate(' + this.x(d.value.date) + ',' + this.y(d.value.users) + ')')
+      .attr('x', 3)
+      .attr('dy', '0.35em')
+      .style('font', '10px sans-serif')
+      .text((d) => d.metric);
 
   }
 
